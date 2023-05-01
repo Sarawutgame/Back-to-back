07aa31d161208d30762e799d14a514aad3aab3dc
Tangkabodee Sathitkasemsan <71114737+TangKBD@users.noreply.github.com>
2023-05-01 23:40:33 +0700

    Add files via upload

--- src/components/Adminpage.js
+++ src/components/Adminpage.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import InputLabel from '@mui/material/InputLabel';
+// import MenuItem from '@mui/material/MenuItem';
+// import FormControl from '@mui/material/FormControl';
+// import Select from '@mui/material/Select';
+
+
+// import Backdrop from '@mui/material/Backdrop';
+// import Box from '@mui/material/Box';
+// import Modal from '@mui/material/Modal';
+// import Fade from '@mui/material/Fade';
+// import Topbar from '../components/Topbar/Topbar';
 import './Adminpage.css'; // Import the CSS file
 
+
+
+
+// function AdminModalButton(){
+//     const [showModalAdmin, setShowModalAdmin] = useState(false);
+
+//     const toggleModal = () => {
+//         setShowModalAdmin(!showModalAdmin);
+//     }
+// }
+
 function AdminPage() {
+
+
+    const [showModalAdmin, setShowModalAdmin] = useState(false);
+
+    const toggleModal = () => {
+        setShowModalAdmin(!showModalAdmin);
+    };
+
+
     return (
 
         <div className="container-ad">
@@ -21,7 +52,20 @@ function AdminPage() {
                         <td>Email not linked</td>
                         <td>Tom Cruise</td>
                         <td>May 26 2019</td>
-                        <td><button className='Ban-ad'>Ban</button></td>
+                        <td>
+                            <button onClick={toggleModal} className='Ban-ad'>Ban</button>
+                            {showModalAdmin && (
+                                <div className='modalAdminBG'>
+                                    <div className='modalAdmin-container'>
+                                        <div className='modalAdminTitle'>
+                                            <h5 style={{ marginBottom: '0px' }}>ต้องการแบนผู้ใช้รายนี้หรือไม่</h5>
+                                        </div>
+                                        <button className='admin-close-button' onClick={toggleModal}>ยกเลิก</button>
+                                        <button className='admin-confrim-button'>ยืนยัน</button>
+                                    </div>
+                                </div>
+                            )}
+                        </td>
 
                     </tr>
                     <tr className="row">
